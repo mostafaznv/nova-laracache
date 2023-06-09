2e4ab75c7bd009044d94b8b29afcd955e72477e6
mostafaznv <mostafa.zeinivand@gmail.com>
2023-06-09 20:23:06 +0330

    cleanup

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,12 +1,5 @@
-/*import DetailField from './fields/DetailField.vue'
-import FormField from './fields/FormField.vue'
-import IndexField from './fields/IndexField.vue'*/
 import Tool from './pages/Tool.vue'
 
 Nova.booting(app => {
     Nova.inertia('NovaLaraCache', Tool)
-
-    /*app.component('index-nova-file-manager-field', IndexField)
-    app.component('detail-nova-file-manager-field', DetailField)
-    app.component('form-nova-file-manager-field', FormField)*/
 })
